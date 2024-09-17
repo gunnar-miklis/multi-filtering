88102a79f1f9d16128dd7c273e291de742cc1623
Gunnar Miklis <gunnar.miklis@gmail.com>
2024-09-17 12:44:58 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Lorem Ipsum</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid neque cumque sint,
+        distinctio libero similique corporis voluptatum qui. Quasi at mollitia, vitae praesentium
+        voluptate inventore repudiandae reiciendis sunt nobis deserunt?
+      </p>
+    </>
+  );
+}
+
+export default App;
